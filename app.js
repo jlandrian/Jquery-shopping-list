39c40e91baec6e6cb4c9e54c51fdb7fb57c56b81
jlandrian <jlandrian@gmail.com>
2014-03-21 14:30:27 -0700

    add and delete button functionality

--- app.js
+++ app.js
@@ -1,8 +1,51 @@
-$(document).ready(function(){
+//<li>'s  will look like this:
+//<li><div class="checkbox"><input type="checkbox" name="grocery"></div>+added+<div class="deletebox"><button>x</button></div><hr></li>
 
-$('.addbox').on('click', (function () {
-	$('ul').append($('li', {
-		text: $('#newitem').val()
-	}));
-}));
+$(document).ready(function(){
+	//set the focus automatically on the text field
+	$('#newitem').focus();
+
+	//a function for adding items to the list
+	function additem() {
+		var added = $("#newitem").val();
+		if (added =="") {
+		alert("Please enter an item first!");
+		}
+		else{
+			$("ul").append("<li><div class='checkbox'><input type='checkbox' name='grocery'></div>"+added+"<div class='deletebox'><button>x</button></div><hr></li>");
+		}
+		$('#newitem').val("")
+					.focus();
+	}
+
+	//a function for deleting items
+	function deleteitem(){
+		$(this).closest('li').remove();
+		console.log('delete clicked');
+	}
+
+	//a function for checking items off the list
+	function checkeditem(){
+		$('#checkbox').closest('li').toggleClass('.selected');
+		console.log('checkbox!!!');
+	}
+
+
+	//add a new item
+	$('.addbox').on('click', 'button', additem);
+	//add a new item on keypress of Enter
+	$('#newitem').on('keydown', function(event){
+		if(event.keyCode == 13){
+			additem();
+			console.log('enter!!!');
+		}
+	});
+
+	//delete an item
+	$('.deletebox').on('click', 'button', deleteitem);
+
+	//check off an item
+	$('#checkbox').on('click', checkeditem);
+		
+	
 });
