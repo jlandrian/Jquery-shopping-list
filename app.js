013e3e664c9770a491a02966ecf9545acd1b36c6
jlandrian <jlandrian@gmail.com>
2014-03-24 18:33:58 -0700

    styled buttons and added placeholder text

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 //<li>'s  will look like this:
 //<li><div class="checkbox"><input type="checkbox" name="grocery"></div>+added+<div class="deletebox"><button>x</button></div><hr></li>
-
+ 
 $(document).ready(function(){
 	//set the focus automatically on the text field
 	$('#newitem').focus();
-
+ 
 	//a function for adding items to the list
 	function additem() {
 		var added = $("#newitem").val();
@@ -12,27 +12,33 @@ $(document).ready(function(){
 		alert("Please enter an item first!");
 		}
 		else{
-			$("ul").append("<li><div class='checkbox'><input type='checkbox' name='grocery'></div>"+added+"<div class='deletebox'><button>x</button></div><hr></li>");
+			$("ul").append("<li><div class='checkbox'><input type='checkbox' name='grocery'></div>"+added+"<div class='deletebox'><button class='deletebtn'>x</button></div><hr></li>");
 		}
 		$('#newitem').val("")
 					.focus();
 	}
-
+ 
 	//a function for deleting items
 	function deleteitem(){
 		$(this).closest('li').remove();
 		console.log('delete clicked');
 	}
+ 
+	//a function to remove all items
+	function removeall(){
+		$('ul').html("");
+
+	}
 
 	//a function for checking items off the list
 	function checkeditem(){
-		$('#checkbox').closest('li').toggleClass('.selected');
+		$(this).closest('li').toggleClass('selected');
 		console.log('checkbox!!!');
 	}
-
-
+ 
 	//add a new item
 	$('.addbox').on('click', 'button', additem);
+	
 	//add a new item on keypress of Enter
 	$('#newitem').on('keydown', function(event){
 		if(event.keyCode == 13){
@@ -40,12 +46,14 @@ $(document).ready(function(){
 			console.log('enter!!!');
 		}
 	});
-
+ 
 	//delete an item
-	$('.deletebox').on('click', 'button', deleteitem);
-
+	$('ul').on('click', '.deletebtn', deleteitem);
+ 
 	//check off an item
-	$('#checkbox').on('click', checkeditem);
-		
-	
+	$('ul').on('click', 'input', checkeditem);	
+
+	//remove all items
+	$('.remove').on('click', removeall);
+
 });
