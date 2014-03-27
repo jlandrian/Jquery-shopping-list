35bcc63e7ce67b0ec154ac7a0262ae8df5520b95
jlandrian <jlandrian@gmail.com>
2014-03-27 11:01:56 -0700

    added trim method to js for adding items

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ $(document).ready(function(){
  
 	//a function for adding items to the list
 	function additem() {
-		var added = $("#newitem").val();
-		if (added ==="") {
+		var added = $.trim($("#newitem").val());
+		if (added =="") {
 		alert("Please enter an item first!");
 		}
 		else{
